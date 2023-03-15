0940820f7468be2a20e32775cac868951870dc28
paxtonBravo <96046448+paxtonBravo@users.noreply.github.com>
2023-03-15 13:23:35 +0800

    first commit

---
+++ app/src/main/java/com/example/google/homework1/MainActivity.java
@@ -0,0 +1,21 @@
+package com.example.google.homework1;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        // change activity
+//        Intent secondActivityIntent;
+//        secondActivityIntent = new Intent(
+//                this, MainActivity2.class
+//        );
+//        startActivity(secondActivityIntent);
+    }
+}
